e7253234fe98340aef45ba499d454d20fe279e6f
Mohammed's School Account <159501876+mcodes450@users.noreply.github.com>
2026-06-14 18:57:03 -0400

    Update and rename 260-HW001.s to CSCI260-HW1.s

--- CSCI260-HW1.s
+++ CSCI260-HW1.s
@@ -1,3 +1,4 @@
+# Mohammed Uddin
 # CSCI 26000 - HW1: 2a + b
 # Register t0 holds a, t1 holds b. Compute 2a + b and put it in t2.
 # Use only `add`. See the assignment description for examples.
@@ -12,7 +13,7 @@ main:
 
 # DONOTMODIFYTHISLINE
 
-  add t2, t0, t0   # Add a to itself to compute 2a
-  add t2, t2, t1   # Add b to the intermediate result, yielding 2a + b
-
+    add t2, t0, t0   # Add a to itself to compute 2a; a + a = 2a -- store it in t2
+    add t2, t2, t1   # Add b (or t1) to the intermediate result (t2), yielding 2a + b
+    
 # DONOTMODIFYTHISLINE
